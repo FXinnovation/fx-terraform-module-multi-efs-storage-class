4e63f4880f91a0e142b50bd2c090cd92d522668b
David Glantenay <david.glantenay@fxinnovation.com>
2021-06-11 14:52:44 -0400

    feat: Upgrade to version 0.14 for terraform and bump providers to latest versions

--- examples/default/providers.tf
+++ examples/default/providers.tf
@@ -1,12 +1,7 @@
 provider "aws" {
-  version    = "~> 2.31.0"
   region     = "us-east-2"
   access_key = var.access_key
   secret_key = var.secret_key
-}
-
-provider "random" {
-  version = "~> 2.0"
 }
 
 provider "kubernetes" {
@@ -14,6 +9,4 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(module.eks.certificate_authority)
   token                  = data.aws_eks_cluster_auth.this.token
   load_config_file       = false
-  // see https://github.com/terraform-providers/terraform-provider-kubernetes/issues/759
-  version = "1.10.0"
 }
